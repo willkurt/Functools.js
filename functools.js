714d9fe01cd7cc0f745a87a4588277fc5aad981e
Will Kurt <wckurt@gmail.com>
2010-08-18 10:16:03 -0700

    doing some more jslint cleanup

--- functools.js
+++ functools.js
@@ -18,23 +18,23 @@ var FUNCTOOLS = {};
 */
 
 //car
-FUNCTOOLS.first = function(xs){
+FUNCTOOLS.first = function (xs) {
     return xs[0];
 };
 
 
 //cdr
-FUNCTOOLS.rest = function(xs){
+FUNCTOOLS.rest = function (xs) {
     return xs.slice(1);
 };
 
 //cons
-FUNCTOOLS.build = function(x,xs){
+FUNCTOOLS.build = function (x,xs) {
     return [x].concat(xs);
 };
 
 //nil?
-FUNCTOOLS.empty = function(xs){
+FUNCTOOLS.empty = function (xs) {
     return xs.length === 0;
 };
 
@@ -46,7 +46,7 @@ FUNCTOOLS.empty = function(xs){
 
 */
 
-FUNCTOOLS.map = function(func,xs){
+FUNCTOOLS.map = function (func,xs) {
     return(
 	FUNCTOOLS.empty(xs) ? [] :
 	FUNCTOOLS.build(func(FUNCTOOLS.first(xs)),
@@ -55,7 +55,7 @@ FUNCTOOLS.map = function(func,xs){
 };
 
 
-FUNCTOOLS.filter = function(test,xs){
+FUNCTOOLS.filter = function (test,xs) {
     return(
 	FUNCTOOLS.empty(xs) ? []:
 	    test(FUNCTOOLS.first(xs)) ? FUNCTOOLS.build(FUNCTOOLS.first(xs),FUNCTOOLS.filter(test,FUNCTOOLS.rest(xs))) :
@@ -67,7 +67,7 @@ FUNCTOOLS.filter = function(test,xs){
 FUNCTOOLS.foldl is much more representative of how Haskell and my other fp languages
 implement fold left.  Also in the discussion here: http://lists.racket-lang.org/users/archive/2010-August/041037.html  Matthias Felleisen (co-author of Little/Seasoned Schemer) backs up this version, so I'm going to stick with it ;)
 */
-FUNCTOOLS.foldl = function(step, init, xs){
+FUNCTOOLS.foldl = function (step, init, xs) {
     return(
 	FUNCTOOLS.empty(xs) ? init:
 	    FUNCTOOLS.foldl(step, step(init, FUNCTOOLS.first(xs)),FUNCTOOLS.rest(xs))
@@ -84,7 +84,7 @@ FUNCTOOLS.fold_left is based on the Racket implementation of foldl
 see discussion here for more info: http://lists.racket-lang.org/users/archive/2010-August/041037.html
 
 */
-FUNCTOOLS.fold_left = function(step, init, xs){
+FUNCTOOLS.fold_left = function (step, init, xs) {
     return(
 	FUNCTOOLS.empty(xs) ? init:
 	    FUNCTOOLS.fold_left(step, step(FUNCTOOLS.first(xs),init),FUNCTOOLS.rest(xs))
@@ -93,7 +93,7 @@ FUNCTOOLS.fold_left = function(step, init, xs){
 
 
 /*thankfully foldr is less complicated ;) */
-FUNCTOOLS.foldr = function(step, init, xs){
+FUNCTOOLS.foldr = function (step, init, xs) { 
     return(
 	FUNCTOOLS.empty(xs) ? init:
 	    step(FUNCTOOLS.first(xs),FUNCTOOLS.foldr(step, init, FUNCTOOLS.rest(xs)))
@@ -105,14 +105,14 @@ FUNCTOOLS.foldr = function(step, init, xs){
   this is not a traiditional function but it's actually quite useful when combined 
   with functions like foldl.  It shows the power of first class functions, lambdas, and closures.
 */
-FUNCTOOLS.rargs = function(func){
+FUNCTOOLS.rargs = function (func) {
     return function(b,a){
 	return func(a,b);
     };
 };
 
 /*compose is simple but powerful and useful*/
-FUNCTOOLS.compose = function(g,f){
+FUNCTOOLS.compose = function (g,f) {
     return function(x){
 	return g(f(x));
     };
@@ -127,7 +127,7 @@ FUNCTOOLS.compose = function(g,f){
 */
 
 
-FUNCTOOLS.length = function(xs){
+FUNCTOOLS.length = function (xs) {
     return(
 	FUNCTOOLS.empty(xs) ? 0 :
 	1 + FUNCTOOLS.length(FUNCTOOLS.rest(xs))
@@ -136,7 +136,7 @@ FUNCTOOLS.length = function(xs){
 
 /* this version of reverse makes use of 2 of our other abstraction, foldl and rargs*/
 
-FUNCTOOLS.reverse = function(xs){
+FUNCTOOLS.reverse = function (xs) {
     return FUNCTOOLS.foldl(FUNCTOOLS.rargs(FUNCTOOLS.build),[],xs);
 };
 
